6e6496c9407b2699c9238acb7bd1f15b787787b5
Johntremont <piloto2810@gmail.com>
2020-02-19 16:13:59 -0300

    [state-para-las-citas]
    - se agrega state para formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Formulario = () => {
+  // Crear State de Citas
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    fecha: "",
+    hora: "",
+    sintomas: ""
+  });
+
+  // Funcion que se ejecuta cada vez que el usuario escribe en un imput
+  const actualizarState = e => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  //Extraer los valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
   return (
     <Fragment>
       <h2>Crear cita</h2>
@@ -12,6 +31,8 @@ const Formulario = () => {
           name="mascota"
           className="u-full-width"
           placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
         />
 
         <label>Nombre del Dueño</label>
@@ -20,16 +41,35 @@ const Formulario = () => {
           name="propietario"
           className="u-full-width"
           placeholder="Nombre del Dueño"
+          onChange={actualizarState}
+          value={propietario}
         />
 
         <label>Fecha</label>
-        <input type="date" name="fecha" className="u-full-width" />
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width bg-success"
+          onChange={actualizarState}
+          value={fecha}
+        />
 
         <label>Hora</label>
-        <input type="time" name="hora" className="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
 
         <label>Sintomas</label>
-        <textarea className="u-full-width"></textarea>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
 
         <button type="submit" className="u-full-width button-primary">
           Agregar Cita
